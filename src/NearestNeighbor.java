a0e011a45e5c858e64cce88d67073b10fc447bee
bharath-rajakumar <bharath.rajakumar@gmail.com>
2014-10-09 23:46:11 -0500

    Added kNN & euclidean dist measurement

--- src/NearestNeighbor.java
+++ src/NearestNeighbor.java
@@ -3,7 +3,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class NearestNeighbor {
 
@@ -25,6 +26,7 @@ public class NearestNeighbor {
         referenceTable = buildReferenceTable(file2Contents,m);
 
         // Step3: Create folds for a given permutation
+        // for loop for permutation has to be implemented
         String[] permutation1 = file1Lines[1].split(" ");
         ArrayList<ArrayList<Integer>> folds = createKFolds(permutation1, kFold);
         for(ArrayList<Integer> list : folds) {
@@ -45,32 +47,81 @@ public class NearestNeighbor {
                 }
             }
 
+            int kValue = 4; // for loop for all k-NN values has to be implemented
             for(Integer a : testingFold) {
-                String classfication = knnClassification(a, trainingFold);
+                String kNNClassfication = knnClassifier(kValue, a, trainingFold);
+                System.out.println(a + " is classified as " + kNNClassfication + " based on k-NN classification");
+                System.out.println();
             }
-
+            System.out.println("---");
         }
 
     }
 
-    public static String knnClassification(Integer a, ArrayList<Integer> trainingFold) {
+    public static String knnClassifier(Integer k, Integer a, ArrayList<Integer> trainingFold) {
         // find the euclidean distance of each point from point A
-        Double[] dist = euclideanDistance(a, trainingFold);
+        Points[] dist = euclideanDistance(a, trainingFold);
 
+        // Create a priority queue that will store only k shortest Points based on its euclidean distance
+        PriorityQueue<Points> kNearestPoints = new PriorityQueue<Points>(k, new Comparator<Points>() {
+            @Override
+            public int compare(Points p1, Points p2) {
+                if(p1.dist > p2.dist) {
+                    return 1;
+                }
+                if(p1.dist < p2.dist) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
 
-        return "";
+        for(Points p : dist) {
+            if(kNearestPoints.size() == k) {
+                kNearestPoints.add(p);
+                kNearestPoints.poll();
+            } else {
+                kNearestPoints.add(p);
+            }
+        }
+
+        // Find no of positive and negative category
+        int pcount = 0;
+        int ncount = 0;
+        for (Points p : kNearestPoints) {
+            System.out.println("("+p.x+", "+p.y+"), "+p.getDistance()+", " + p.category);
+            if(p.category.equals("+")) {
+              pcount = pcount + 1;
+            }
+
+            if(p.category.equals("-")) {
+                ncount = ncount + 1;
+            }
+        }
+
+        // Classify the point based on number of positives and negatives count
+        if (pcount > ncount) {
+            return "+";
+        } else {
+            return "-";
+        }
     }
 
-    public static Double[] euclideanDistance(Integer a, ArrayList<Integer> trainingFold) {
-        Double[] dist = new Double[trainingFold.size()];
+    public static Points[] euclideanDistance(Integer a, ArrayList<Integer> trainingFold) {
+        Points[] dist = new Points[trainingFold.size()];
         int x1 = Integer.parseInt(referenceTable[a][1]);
         int y1 = Integer.parseInt(referenceTable[a][2]);
+        Points one = new Points(x1, y1);
+        one.dist = 0.0;
+        one.category = referenceTable[a][3];
         int i = 0;
         for(Integer point : trainingFold) {
             int x2 = Integer.parseInt(referenceTable[point][1]);
             int y2 = Integer.parseInt(referenceTable[point][2]);
-            double tempDist = Math.pow((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)), 0.5);
-            dist[i] = tempDist;
+            Points two = new Points(x2, y2);
+            two.dist = Math.pow((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)), 0.5);
+            two.category = referenceTable[point][3];
+            dist[i] = two;
             i++;
         }
         return dist;
@@ -170,5 +221,29 @@ public class NearestNeighbor {
             e.printStackTrace();
         }
         return contents;
+    }
+}
+
+class Points {
+    int x = 0;
+    int y = 0;
+    double dist = 0.0;
+    String category = "";
+
+    Points(int a, int b) {
+        this.x = a;
+        this.y = b;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getX() {
+        return this.y;
+    }
+
+    public double getDistance() {
+        return this.dist;
     }
 }
